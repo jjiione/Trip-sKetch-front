0d45891b865367cd0f578aa623950457a35fda72
jiwon <jiwonjeong37@gmail.com>
2023-05-18 18:03:54 +0900

    :lipstick: [Style] main 페이지 생성

--- trip-sketch/src/components/layout/HeaderNaviBar.vue
+++ trip-sketch/src/components/layout/HeaderNaviBar.vue
@@ -1,46 +1,46 @@
 <template>
-	<div class="nav-bar-id">
-		<div class="container">
-			<b-navbar class="nav-bar-id" type="dark">
-				<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  <div class="nav-bar-id">
+    <div class="container">
+      <b-navbar class="nav-bar-id" type="dark">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-				<b-collapse id="nav-collapse" is-nav>
-					<b-navbar-nav>
-						<b-nav-item style="margin: 10px 10px" href="#">Link</b-nav-item>
-						<b-nav-item style="margin: 10px 10px" href="#">Disabled</b-nav-item>
-					</b-navbar-nav>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item style="margin: 10px 10px" href="#">Home</b-nav-item>
+            <b-nav-item style="margin: 10px 10px" href="#">Disabled</b-nav-item>
+          </b-navbar-nav>
 
-					<b-navbar-nav class="ml-auto">
-						<img src="@/assets/nightlogo.gif" style="width: 100px; width: 100px" />
-					</b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <img src="@/assets/nightlogo.gif" style="width: 100px; width: 100px" />
+          </b-navbar-nav>
 
-					<!-- Right aligned nav items -->
-					<b-navbar-nav class="ml-auto">
-						<b-nav-item-dropdown text="Lang" style="margin: 10px 10px">
-							<b-dropdown-item href="#">EN</b-dropdown-item>
-							<b-dropdown-item href="#">ES</b-dropdown-item>
-							<b-dropdown-item href="#">RU</b-dropdown-item>
-							<b-dropdown-item href="#">FA</b-dropdown-item>
-						</b-nav-item-dropdown>
-						<b-nav-item style="margin: 10px 10px"
-							><router-link :to="{ name: 'boardlist' }" style="color: white"
-								>Community</router-link
-							></b-nav-item
-						>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown text="Lang" style="margin: 10px 10px">
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item style="margin: 10px 10px"
+              ><router-link :to="{ name: 'boardlist' }" style="color: white"
+                >Community</router-link
+              ></b-nav-item
+            >
 
-						<b-nav-item-dropdown right style="margin: 10px 10px">
-							<!-- Using 'button-content' slot -->
-							<template #button-content>
-								<em>User</em>
-							</template>
-							<b-dropdown-item href="#">Profile</b-dropdown-item>
-							<b-dropdown-item href="#">Sign Out</b-dropdown-item>
-						</b-nav-item-dropdown>
-					</b-navbar-nav>
-				</b-collapse>
-			</b-navbar>
-		</div>
-	</div>
+            <b-nav-item-dropdown right style="margin: 10px 10px">
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -49,10 +49,10 @@ export default {};
 
 <style>
 .nav-bar-id {
-	background-color: rgb(39, 39, 39);
+  background-color: rgb(39, 39, 39);
 }
 
 b-nav-item {
-	color: white;
+  color: white;
 }
 </style>
