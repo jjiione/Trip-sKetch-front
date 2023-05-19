e41eead8ca9d92c0e87b0a76571ea08ab6a1da18
jiwon <jiwonjeong37@gmail.com>
2023-05-19 13:28:57 +0900

    :rocket: [Feat] section2,3 및 카카오 지도 불러오기

--- trip-sketch/src/views/main/MainPage.vue
+++ trip-sketch/src/views/main/MainPage.vue
@@ -2,17 +2,20 @@
   <div>
     <dark-main-section-1></dark-main-section-1>
     <dark-main-section-2></dark-main-section-2>
+    <dark-main-section-3></dark-main-section-3>
   </div>
 </template>
 
 <script>
-import DarkMainSection1 from "@/components/main/DarkMainSection1.vue";
-import DarkMainSection2 from "@/components/main/DarkMainSection2.vue";
+import DarkMainSection1 from "@/components/main/dark/DarkMainSection1.vue";
+import DarkMainSection2 from "@/components/main/dark/DarkMainSection2.vue";
+import DarkMainSection3 from "@/components/main/dark/DarkMainSection3.vue";
 export default {
   name: "MainPage",
   components: {
     DarkMainSection1,
     DarkMainSection2,
+    DarkMainSection3,
   },
 };
 </script>
