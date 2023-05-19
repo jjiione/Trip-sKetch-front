ed506ce75b34f943595b2c04d03370329f6bdb45
jiwon <jiwonjeong37@gmail.com>
2023-05-19 15:19:46 +0900

    :lipstick: [Style] main페이지 수정 및 font 적용

--- trip-sketch/src/components/main/dark/DarkMainSection1.vue
+++ trip-sketch/src/components/main/dark/DarkMainSection1.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="position: relative">
+  <div style="position: relative" id="dark-main-section1-form">
     <div style="background-color: black; vertical-align: middle">
       <img id="main-night-gif" src="@/assets/night.gif" />
     </div>
@@ -10,7 +10,7 @@
         text-align: center;
         text-align: center;
         position: absolute;
-        top: 30%;
+        top: 25%;
         left: 50%;
         transform: translate(-50%, -50%);
       "
@@ -18,7 +18,27 @@
       <span
         v-for="(t, index) in text1"
         :key="index"
-        class="item"
+        class="item1"
+        :style="{ animationDelay: index * 100 + 'ms' }"
+        v-text="t"
+      />
+    </div>
+
+    <div
+      class="TextAnime1"
+      style="
+        text-align: center;
+        text-align: center;
+        position: absolute;
+        top: 45%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      "
+    >
+      <span
+        v-for="(t, index) in text2"
+        :key="index"
+        class="item2"
         :style="{ animationDelay: index * 100 + 'ms' }"
         v-text="t"
       />
@@ -31,7 +51,8 @@ export default {
   name: "section-1",
   data() {
     return {
-      text1: "Sketch your Trip",
+      text1: "Trip's ketch",
+      text2: "Sketch your Trip",
     };
   },
 };
@@ -50,11 +71,31 @@ export default {
     opacity: 0;
   }
 }
-.item {
+.item1 {
   display: inline-block;
   min-width: 0.3em;
-  font-size: 2rem;
+  font-size: 7rem;
   animation: text-in 0.8s cubic-bezier(0.22, 0.15, 0.25, 1.43) 0s backwards;
   color: white;
+}
+
+.item2 {
+  display: inline-block;
+  min-width: 0.3em;
+  font-size: 4rem;
+  animation: text-in 0.8s cubic-bezier(0.22, 0.15, 0.25, 1.43) 0s backwards;
+  color: gray;
+}
+
+@font-face {
+  font-family: "yleeMortalHeartImmortalMemory";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2205@1.0/yleeMortalHeartImmortalMemory.woff2")
+    format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+
+#dark-main-section1-form {
+  font-family: "yleeMortalHeartImmortalMemory";
 }
 </style>
