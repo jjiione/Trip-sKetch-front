02caa43c5f69490f5fbe2e84e12417db300533b3
jiwon <jiwonjeong37@gmail.com>
2023-05-19 14:07:31 +0900

    :lipstick: [Style] Header 수정

--- trip-sketch/src/components/layout/DarkHeaderNaviBar.vue
+++ trip-sketch/src/components/layout/DarkHeaderNaviBar.vue
@@ -6,10 +6,6 @@
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item style="margin: 0px 10px 0px 50px">
-              <router-link :to="{ name: 'MainPage' }" style="color: gray">Home</router-link>
-            </b-nav-item>
-
             <b-nav-item-dropdown style="margin: 0px 10px 0px 50px" text="Place">
               <b-dropdown-item>Search</b-dropdown-item>
               <b-dropdown-item>Recommend</b-dropdown-item>
@@ -19,14 +15,7 @@
               <b-dropdown-item href="#">Plan Create</b-dropdown-item>
               <b-dropdown-item href="#">My Plan List</b-dropdown-item>
             </b-nav-item-dropdown>
-          </b-navbar-nav>
 
-          <b-navbar-nav class="ml-auto">
-            <img src="@/assets/nightlogo.gif" style="width: 100px; width: 100px" />
-          </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
             <b-nav-item-dropdown text="community" style="margin: 0px 10px 0px 50px">
               <b-dropdown-item>
                 <router-link :to="{ name: 'boardlist' }" style="color: gray"
@@ -35,7 +24,16 @@
               </b-dropdown-item>
               <b-dropdown-item>공지 사항</b-dropdown-item>
             </b-nav-item-dropdown>
+          </b-navbar-nav>
 
+          <b-navbar-nav class="ml-auto">
+            <router-link :to="{ name: 'MainPage' }" style="color: gray">
+              <img src="@/assets/nightlogo.gif" style="width: 100px; width: 100px" />
+            </router-link>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
             <b-nav-item style="margin: 0px 10px 0px 50px">
               <router-link :to="{ name: 'MainPage' }" style="color: gray">Login</router-link>
             </b-nav-item>
