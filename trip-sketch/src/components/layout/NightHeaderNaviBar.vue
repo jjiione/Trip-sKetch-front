ddc73c57b0d55176b59da28cafb24d40f9c761e5
jiwon <jiwonjeong37@gmail.com>
2023-05-19 09:49:19 +0900

    :lipstick: [Style] header toggle switch 생성

--- trip-sketch/src/components/layout/NightHeaderNaviBar.vue
+++ trip-sketch/src/components/layout/NightHeaderNaviBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav-bar-id">
     <div class="container">
-      <b-navbar class="nav-bar-id" type="dark">
+      <b-navbar class="nav-bar-id" type="light">
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
@@ -22,7 +22,7 @@
           </b-navbar-nav>
 
           <b-navbar-nav class="ml-auto">
-            <img src="@/assets/nightlogo.gif" style="width: 100px; width: 100px" />
+            <img src="@/assets/daylogo.gif" style="width: 100px; width: 100px" />
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
@@ -46,36 +46,85 @@
               <b-dropdown-item href="#">RU</b-dropdown-item>
               <b-dropdown-item href="#">FA</b-dropdown-item>
             </b-nav-item-dropdown>
-
-            <b-form-checkbox
-              style="margin: 10px 10px 0px 50px"
-              class="nav-color"
-              v-model="checked"
-              name="check-button"
-              switch
-            >
-              mode
-            </b-form-checkbox>
           </b-navbar-nav>
         </b-collapse>
+
+        <!-- Rounded switch -->
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider round"></span>
+        </label>
       </b-navbar>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      checked: false,
-    };
-  },
-};
+export default {};
 </script>
 
 <style scoped lang="scss">
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #2196f3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
 .nav-bar-id {
-  background-color: black;
+  background-color: white;
 }
 
 #nav-margin {
@@ -83,7 +132,7 @@ export default {
 }
 ::v-deep {
   .nav-color {
-    color: white !important;
+    color: skyblue !important;
   }
 }
 </style>
