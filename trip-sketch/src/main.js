a6a434eab8c5dbe25e0b2b36601be12dd8c060ac
jiwon <jiwonjeong37@gmail.com>
2023-05-23 10:51:06 +0900

    :lipstick: [Style] 로그인 설정 중

--- trip-sketch/src/main.js
+++ trip-sketch/src/main.js
@@ -9,6 +9,24 @@ import Axios from "axios";
 window.Swal = swal;
 window.axios = Axios;
 
+  // Import the functions you need from the SDKs you need
+import firebase from 'firebase'
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId,
+	measurementId: process.env.measurementId,
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 // import VueLazyload from "vue-lazyload";
 
 Vue.config.productionTip = false;
