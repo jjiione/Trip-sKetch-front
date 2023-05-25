3fca6a66accf3caa96801b5dec490ecb3667c9d4
jiwon <jiwonjeong37@gmail.com>
2023-05-25 17:50:43 +0900

    :rocket: [Feat] 이미지 등록 완료

--- trip-sketch/src/components/board/BoardCreate.vue
+++ trip-sketch/src/components/board/BoardCreate.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="board_wrap">
 		<div class="regist">
-			<div class="board_title"><h2 style="font-weight: bold">글 작성</h2></div>
+			<div class="board_title">
+				<h2 style="font-weight: bold">글 작성</h2>
+			</div>
 			<div class="regist_form">
 				<div class="board_write_wrap">
 					<div class="board_write">
@@ -19,20 +21,14 @@
 							<dl>
 								<dt>파일 첨부</dt>
 								<dd>
-									<input type="text" id="fileInfos" ref="fileInfos" v-model="article.fileInfos" />
+									<input id="file-selector" ref="file" type="file" @change="handleFileUpload()" />
 								</dd>
+								<!-- <button @click="uploadFile" color="primary">button</button> -->
 							</dl>
 						</div>
 						<div class="cont">
-							<textarea
-								class="cont"
-								placeholder="내용 입력"
-								id="content"
-								ref="content"
-								cols="35"
-								rows="5"
-								v-model="article.content"
-							></textarea>
+							<textarea class="cont" placeholder="내용 입력" id="content" ref="content" cols="35" rows="5"
+								v-model="article.content"></textarea>
 						</div>
 					</div>
 					<div class="bt_wrap">
@@ -47,6 +43,7 @@
 
 <script>
 import axios from "axios";
+import AWS from 'aws-sdk';
 const url = "http://localhost:80/board/notice/regist";
 export default {
 	name: "BoardCreate",
@@ -62,6 +59,16 @@ export default {
 				boardType: "",
 				fileInfos: "",
 			},
+			file: null,
+
+			albumBucketName: 'trips-ketch',
+			bucketRegion: 'ap-northeast-2',
+			IdentifyPoolId: 'ap-northeast-2:fdcfec3c-d67d-4491-ac5e-1f9ba8ad1777',
+			// albumBucketName: String,
+			// bucketRegion: String,
+			// IdentifyPoolId: String,
+
+
 		};
 	},
 	methods: {
@@ -84,6 +91,10 @@ export default {
 			else this.registArticle();
 		},
 		async registArticle() {
+			this.uploadFile();
+
+
+
 			// 비동기
 			// TODO : 글번호에 해당하는 글정보 등록.
 			let articles = new Array();
@@ -94,6 +105,8 @@ export default {
 				title: this.article.title,
 				content: this.article.content,
 				userId: this.article.userId,
+				img: 'ap-northeast-2:fdcfec3c-d67d-4491-ac5e-1f9ba8ad1777' + this.file.name,
+
 				// fileInfos: this.article.fileInfos,
 			});
 			this.$router.push({ name: "boardlist" });
@@ -102,6 +115,43 @@ export default {
 		moveList() {
 			this.$router.push({ name: "boardlist" });
 		},
+		handleFileUpload() {
+			this.file = this.$refs.file.files[0]
+			console.log("file select");
+		},
+		uploadFile() {
+			AWS.config.update({
+				region: this.bucketRegion,
+				credentials: new AWS.CognitoIdentityCredentials({
+					IdentityPoolId: 'ap-northeast-2:fdcfec3c-d67d-4491-ac5e-1f9ba8ad1777',
+
+				})
+			});
+
+			var s3 = new AWS.S3({
+				apiVersion: "2006-03-01",
+				params: {
+					Bucket: this.albumBucketName
+				}
+			});
+
+			let photoKey = this.file.name;
+
+			s3.upload({
+				Key: photoKey,
+				Body: this.file,
+				ACL: 'public-read'
+			}, (err, data) => {
+				if (err) {
+					console.log(err);
+					return alert("파일 업로드 실패!!");
+				}
+				alert("파일 업로드 성공");
+				console.log(data);
+			});
+		},
+
+
 	},
 };
 </script>
